2085009d56fb0ee13ff6fca481794bb4adab47e0
Shreya Deb <76243929+shreyaDeb@users.noreply.github.com>
2022-11-19 19:18:05 -0700

    Add files via upload

---
+++ client/src/components/AddEntry.jsx
@@ -0,0 +1,62 @@
+import { useState, useRef } from 'react';
+import axios from 'axios'
+
+const AddEntry = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [volunteerList, setVolunteerList] = useState([])
+
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+
+  // CREATE (POST)
+  function submitEntry() {
+    axios.post(`${process.env.REACT_APP_HOST}/api/create`, { first: firstName, last: lastName, email: emailAddress }).then((response) => {
+      setVolunteerList([...volunteerList, { first_name: firstName, last_name: lastName, email_address: emailAddress }]
+      )
+    })
+
+    ref1.current.value = ""
+    setFirstName('')
+    ref2.current.value = ""
+    setLastName('')
+    ref3.current.value = ""
+    setEmailAddress('')
+  }
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  return (
+    <div className="addEntry">
+      <h2>Add an Entry</h2>
+      <div id='userInput'>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input ref={ref1} id="firstName" type="text" name="firstName" onChange={(e) => setFirstName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input ref={ref2} id="lastName" type="text" name="lastName" onChange={(e) => setLastName(e.target.value)} />
+        </div><br />
+        <div className="emailField" >
+          <label htmlFor="email">Email Address</label>
+          <input ref={ref3} id="email" type="email" name="email" onChange={(e) => setEmailAddress(e.target.value)} />
+        </div>
+        <button className="submitBtn"
+          onClick={() => {
+            if (firstName.length > 0 && lastName.length > 0 && emailAddress.length > 0) {
+              submitEntry(); refreshPage();
+            }
+          }}
+        >Add Entry</button>
+      </div>
+    </div>
+  )
+}
+
+export default AddEntry;
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <h2>Footer</h2>
+            <p><em> Shreya Deb, j15m224</em></p>
+            <p>My role in the project: </p>
+            <ul>
+                <li>Backend: loading tickets into DB from a given spreadsheet</li>
+                <li>Frontend: In case we don't use bootstrap I will be Standardized CSS Classes for reused components</li>
+                <li> Ports: 3040 and 5040</li>
+                </ul>
+        </div>
+    )
+}
+export default Footer
